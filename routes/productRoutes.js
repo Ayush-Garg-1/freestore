156ac6b9a7af3e18e4544a8d1616fe59cbb411ae
Ayush501Garg <ayush501bmt@gmail.com>
2025-08-06 14:41:16 +0530

    6-08

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -13,26 +13,23 @@ const {
     searchProducts,
 } = require('../controllers/productController');
 
-
-// Load Data
-const { loadProductData } = require('../controllers/loadProductData');
+// Load Data (Optional)
+// const { loadProductData } = require('../controllers/loadProductData');
 // router.get('/loadData', loadProductData);
 
-
 // CRUD routes
+router.get('/category', getCategories);
+router.get('/category/:category', getProductsByCategory);
+router.get('/search', searchProducts);
+
 router.get('/', getProducts);
-router.get('/:id', getProductById);
 router.post('/', createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
+router.get('/:id', getProductById);
+
 // Image upload route
 router.post('/upload', upload.single('image'), uploadImage);
-
-router.get('/categories', getCategories);
-router.get('/category/:category', getProductsByCategory);
-router.get('/search', searchProducts);
-
-
 
 module.exports = router;
